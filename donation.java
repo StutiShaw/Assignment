c02cd16302ce38685f5dff76ba522e875ac8af60
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-28 09:37:33 +0530

    Update donation.java

--- donation.java
+++ donation.java
@@ -3,18 +3,18 @@ public class donation
 {
   public static void main(String[] args)
   {
-    int age,wgt;
-	Scanner sc=new Scanner(System.in);
-	System.out.print("enter your age :");
+    int age,wgt; //declaration of variables
+	Scanner sc=new Scanner(System.in); // create scanner
+	System.out.print("enter your age :");//to print statement
 	age=sc.nextInt();
-	System.out.print("enter your weight :");
+	System.out.print("enter your weight :");//to print statement
 	wgt=sc.nextInt();
-	if(age>=25)
+	if(age>=25) // if statement
 	{
 	  if(wgt>=48)
-	System.out.print("eligible to donate blood");
+	System.out.print("eligible to donate blood");//to print statement
 	else
-	System.out.print("not eligible to donate blood");
+	System.out.print("not eligible to donate blood");//to print statement
 	}
   }
 }
